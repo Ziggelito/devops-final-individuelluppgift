c9ee6dec4c5b8b39eb7dace2fc9e3668d8913ade
Ziggelito <Sixten.Lonnberg@yh.nackademin.se>
2023-10-05 10:45:53 +0200

    changes

--- src/main/java/com/example/gruppuppgift/CalculatorController.java
+++ src/main/java/com/example/gruppuppgift/CalculatorController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorController {
 
         @RequestMapping("/add")
-        public double useAddition(double d1, double d2){
+        public double useMathAddition(double d1, double d2){
 
             double answer = Calculator.addition(d1, d2);
             return answer;
         }
         @RequestMapping("/subtract")
-        public double useSubtraction(double d1, double d2){
+        public double useMathSubtraction(double d1, double d2){
 
             double answer = Calculator.subtraction(d1, d2);
 
             return answer;
         }
         @RequestMapping("/multiply")
-        public double useMultiplication(double d1, double d2){
+        public double useMathMultiplication(double d1, double d2){
 
             double answer = Calculator.multiplication(d1, d2);
             return answer;
         }
         @RequestMapping("/divide")
-        public double useDivision(double d1, double d2){
+        public double useMathDivision(double d1, double d2){
 
             double answer = Calculator.division(d1, d2);
             return answer;
